5b93eac03481416db17d3532243ca2ad3457263f
kartik <kartikanand1992@gmail.com>
2018-07-08 15:15:42 +0530

    add colors; add start/stop

--- src/scripts/board.js
+++ src/scripts/board.js
@@ -93,6 +93,14 @@ function getNextState (world) {
     return nextState;
 }
 
+/*
+ * get a random h*w initial board
+ * we use Math.random to
+ *
+ * @param {int} h height
+ * @param {int} w width
+ * @return {bool[][]} h*w board
+ */
 function getRandomBoard (h, w) {
     const board = [];
 
@@ -109,19 +117,34 @@ function getRandomBoard (h, w) {
     return board;
 }
 
+/*
+ * get a random integer between 0 and max-1 (inclusive)
+ *
+ * @param {int} max
+ * @return {int} random no. between [0, max)
+ */
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 export default class Board extends React.Component {
     constructor (props) {
         super(props);
 
-        const board = getRandomBoard(30, 30);
+        this.height = window.innerWidth;
+        this.width = window.innerWidth;
+
+        const board = getRandomBoard(this.height/10, this.width/10);
 
         this.state = {
             board: board
         };
 
         this.canvas = React.createRef();
-        this.height = 300;
-        this.width = 300;
+
+        // bind functions
+        this.startSimulation = this.startSimulation.bind(this);
+        this.stopSimulation = this.stopSimulation.bind(this);
     }
 
     updateCells () {
@@ -131,26 +154,38 @@ export default class Board extends React.Component {
         const h = board.length;
         const w = board[0].length;
 
+        const colors = [
+            '#f1c40f',
+            '#3498db',
+            '#1abc9c',
+            '#8e44ad',
+            '#2c3e50',
+            '#fd79a8',
+            '#fab1a0',
+            '#e84393',
+            '#a29bfe',
+            '#fdcb6e'
+        ];
+
         for (let i=0; i < h; i++) {
             for (let j=0; j < w; j++) {
+                const idx = getRandomInt(colors.length);
+
                 if (board[i][j])
-                    ctx.fillStyle = 'red';
+                    ctx.fillStyle = colors[idx];
                 else
-                    ctx.fillStyle = 'blue';
+                    ctx.fillStyle = 'black';
 
                 ctx.fillRect(j*10, i*10, 10, 10);
             }
         }
     }
 
-    componentDidMount () {
-        const ctx = this.canvas.current.getContext('2d');
+    stopSimulation () {
+        clearInterval(this.timerID);
+    }
 
-        ctx.fillStyle = 'black';
-        ctx.fillRect(0, 0, this.height, this.width);
-
-        this.updateCells();
-
+    startSimulation () {
         this.timerID = setInterval(
             () => {
                 this.setState((prevState, props) => {
@@ -162,13 +197,31 @@ export default class Board extends React.Component {
         );
     }
 
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    componentDidMount () {
+        const ctx = this.canvas.current.getContext('2d');
+
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, this.height, this.width);
+
+        this.updateCells();
+    }
+
     componentDidUpdate() {
         this.updateCells();
     }
 
     render () {
         return (
-            <canvas ref={this.canvas} width={this.width} height={this.height} />
+            <React.Fragment>
+                <button onClick={this.startSimulation}>Start</button>
+                <button onClick={this.stopSimulation}>Stop</button>
+
+                <canvas ref={this.canvas} width={this.width} height={this.height} />
+            </React.Fragment>
         );
     }
 }
